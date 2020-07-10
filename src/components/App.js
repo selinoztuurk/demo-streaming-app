8f623e0b260d7024367ffc64ec7dff5a91a648bd
Selin <selinozturk@Selin-MacBook-Pro.local>
2020-07-10 17:49:01 +0300

    SearchBar completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import content from "../feed/sample.json";
 import ContentList from "./ContentList";
 
 class App extends React.Component {
-  state = { content: [] };
+  state = { content: [], errorMessage: "" };
 
   onTermSubmit = (term) => {
     const response = content.entries.filter((entry) => {
@@ -26,16 +26,26 @@ class App extends React.Component {
     }
   };
 
+  renderContent() {
+    if (this.state.errorMessage && this.state.content.length == 0) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.content.length >= 0) {
+      return (
+        <div className="ui container">
+          <SearchBar
+            onFormSubmit={this.onTermSubmit}
+            onInputChange={this.onTermChange}
+          />
+          <ContentList content={this.state.content} />
+        </div>
+      );
+    }
+    return <div>Loading...</div>;
+  }
+
   render() {
-    return (
-      <div className="ui container">
-        <SearchBar
-          onFormSubmit={this.onTermSubmit}
-          onInputChange={this.onTermChange}
-        />
-        <ContentList content={this.state.content} />
-      </div>
-    );
+    return <div>{this.renderContent()}</div>;
   }
 }
 
