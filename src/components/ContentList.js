6d35be0cd48aa8a4d8683421d8c33ff2d8fda8a4
Selin <selinozturk@Selin-MacBook-Pro.local>
2020-07-20 18:12:17 +0300

    minor ui fix

--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -6,7 +6,10 @@ const ContentList = ({ content, onClick }) => {
     return <ContentItem key={cont.title} cont={cont} onClick={onClick} />;
   });
   return (
-    <div className="ui container" style={{ marginTop: "1rem" }}>
+    <div
+      className="ui container"
+      style={{ marginTop: "1rem", marginBottom: "1rem" }}
+    >
       <div className="ui stackable seven column grid">{renderedList}</div>
     </div>
   );
